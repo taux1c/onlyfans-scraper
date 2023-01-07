051765a1852c29d1ccbfa15245d4f87c21617f80
taux1c <24677318+taux1c@users.noreply.github.com>
2023-01-07 09:27:18 -0500

    Add purchased_contentEP to add ability to save purchased content.

--- onlyfans_scraper/constants.py
+++ onlyfans_scraper/constants.py
@@ -42,6 +42,8 @@ postURL = 'https://onlyfans.com/{}/{}'
 
 DC_EP = 'https://raw.githubusercontent.com/DATAHOARDERS/dynamic-rules/main/onlyfans.json'
 
+purchased_contentEP = "https://onlyfans.com/api2/v2/posts/paid?limit=10&skip_users=all&format=infinite&offset={}"
+
 mainPromptChoices = {
     'Download content from a user': 0,
     'Like all of a user\'s posts': 1,
